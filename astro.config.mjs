52d427687b78b26bf9bd1def9dc1ed497cea6b4f
Todd Bittner <tbittner@coastalchicago.com>
2026-02-19 15:45:39 -0600

    Starting attempt to inline Astro code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/worldly',
   trailingSlash: 'ignore',
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es', 'fr'],
-    routing: {
-      prefixDefaultLocale: true,
-      fallbackType: 'redirect'
-    }
-  },
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(),viteSingleFile()],
+    build: {
+      cssCodeSplit: false,
+    }
   }
 });
